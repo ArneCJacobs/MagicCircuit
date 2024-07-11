46bbba55c252dea5f5aeadd911e547c11d0d42f3
Arne <arne.carla.jacobs@outlook.com>
2024-07-11 10:33:00 +0200

    fixed byte offset bug

--- src/lambda_calculus_parser.rs
+++ src/lambda_calculus_parser.rs
@@ -1,11 +1,11 @@
-use std::{sync::{Mutex, OnceLock}, collections::VecDeque};
+use std::{
+    collections::VecDeque,
+    sync::{Mutex, OnceLock},
+};
 
+use itertools::Itertools;
 use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_while_m_n, take_while1},
-    combinator::{map_res, opt, all_consuming},
-    sequence::{delimited, preceded, Tuple},
-    IResult, Parser, multi::many1,
+    branch::alt, bytes::complete::{tag, take_while, take_while1, take_while_m_n}, character::complete::space0, combinator::{all_consuming, map_res, opt}, multi::{many0, many1}, sequence::{delimited, preceded, separated_pair, Tuple}, IResult, Parser
 };
 
 fn indent_reset() {
@@ -70,13 +70,21 @@ enum LambdaExpression {
     Application(Box<LambdaExpression>, Box<LambdaExpression>),
 }
 
-fn parse_lambda_variable(expr: &str) -> IResult<&str, LambdaExpression> {
+fn parse_string_variable(expr: &str) -> IResult<&str, &str> {
     inc_indent();
     defer!(dec_indent());
     println_with_indent("parsing_variable");
     println_with_indent(&format!("here0: {:?}", expr));
     let (input, variable) = take_while_m_n(1, 1, |c: char| c.is_alphabetic())(expr)?;
     println_with_indent(&format!("here1: {:?}", input));
+
+    Ok((input, variable))
+}
+
+fn parse_lambda_variable(expr: &str) -> IResult<&str, LambdaExpression> {
+    inc_indent();
+    defer!(dec_indent());
+    let (input, variable) = parse_string_variable(expr)?;
 
     Ok((input, LambdaExpression::Variable(variable.to_string())))
 }
@@ -110,13 +118,43 @@ fn parse_string_balanced_expression(expr: &str) -> IResult<&str, &str> {
     defer!(dec_indent());
     println_with_indent("parsing_balanced_expression");
     println_with_indent(&format!("here0: {:?}", expr));
-    let (prefix, _) = take_while1(|c: char| c != '(')(expr)?;
-    println_with_indent(&format!("here1: {:?}", prefix));
-    let (input, expr) = delimited(tag("("), parse_string_balanced_expression, tag(")"))(prefix)?;
-    println_with_indent(&format!("here2: {:?}", input));
-    let (postfix, rest) = take_while1(|c: char| c != ')')(input)?;
-    println_with_indent(&format!("here3: {:?}", postfix));
-    Ok((rest, &expr[expr.len() - rest.len()..]))
+    if expr.is_empty() {
+        return Err(nom::Err::Error(nom::error::Error::new(
+            expr,
+            nom::error::ErrorKind::IsNot,
+        )));
+    }
+    let mut count = 0;
+
+    for (index, ch) in expr.char_indices() {
+        match ch {
+            '(' => count += 1,
+            ')' => {
+                if count == 0 {
+                    println_with_indent("more closed brackets then open ones");
+                    // print currrent char and index
+                    //println_with_indent(&format!("index: {:?}, char: {:?}", index, ch));
+                    println_with_indent(&format!("parsed so far: {:?}, rest: {:?}", &expr[..index], &expr[index..]));
+                    //println_with_indent(&format!("parsed so far: {:?}, rest: {:?}", &chars[..index], &chars[index..]));
+                    return Ok((&expr[index..], &expr[..index]));
+                }
+
+                count -= 1;
+            }
+            _ => (),
+        }
+    }
+    println_with_indent(&format!("count: {:?}", count));
+    if count != 0 {
+        println_with_indent(&format!("expression not balanced: {:?}", expr));
+        return Err(nom::Err::Error(nom::error::Error::new(
+            expr,
+            nom::error::ErrorKind::IsNot,
+        )));
+    }
+    println_with_indent(&format!("here1: {:?}", expr));
+
+    Ok(("", expr))
 }
 
 fn parse_lambda_application(expr: &str) -> IResult<&str, LambdaExpression> {
@@ -129,22 +167,42 @@ fn parse_lambda_application(expr: &str) -> IResult<&str, LambdaExpression> {
     //}
     defer!(dec_indent());
     println_with_indent("parsing_application");
-    let parser = preceded(opt(tag(" ")), alt((
-        take_while_m_n(1, 1, |c: char| c.is_alphabetic()),   
-        parse_string_balanced_expression,
-    )));
-    let (input, res) = many1(parser).parse(expr)?;
+    println_with_indent(&format!("here0: {:?}", expr));
+    //let parser = alt((
+    //    parse_string_variable,
+    //    parse_string_balanced_expression,
+    //));
+    //println_with_indent(&format!("here0: {:?}", many1(parser).parse(expr)));
+
+    let parser = preceded(
+        space0, 
+        alt((
+            parse_string_variable,
+            delimited(tag("("), parse_string_balanced_expression, tag(")")),
+        ))
+    );
+    let (input, res) = all_consuming(many1(parser)).parse(expr)?;
+    println_with_indent(&format!("here1: {:?}", input));
+    println_with_indent(&format!("here1 res: {:?}", res));
     if res.len() < 2 {
+        println_with_indent("not enough expressions");
         return Err(nom::Err::Error(nom::error::Error::new(
             expr,
             nom::error::ErrorKind::IsNot,
         )));
     }
-    println_with_indent(&format!("here1: {:?}", input));
-    println_with_indent(&format!("here1 res: {:?}", res));
-    
-    let parsed_expressions = res.iter()
-        .map(|s| all_consuming(parse_lambda_expression).parse(s))
+    println_with_indent(&format!("here1.5: {:?}", res));
+    let parsed_expressions = res
+        .iter()
+        .map(|s| 
+            //all_consuming(
+                alt((
+                    all_consuming(parse_lambda_variable),
+                    all_consuming(parse_lambda_expression),
+                ))
+            //)
+            .parse(s)
+        )
         .collect::<Result<Vec<_>, _>>()?;
     println_with_indent(&format!("here2: {:?}", parsed_expressions));
     let mut expressions = parsed_expressions
@@ -156,65 +214,21 @@ fn parse_lambda_application(expr: &str) -> IResult<&str, LambdaExpression> {
     while expressions.len() > 1 {
         let expr1 = expressions.pop_front().unwrap();
         let expr2 = expressions.pop_front().unwrap();
-        expressions.push_front(LambdaExpression::Application(Box::new(expr1), Box::new(expr2)));
+        expressions.push_front(LambdaExpression::Application(
+            Box::new(expr1),
+            Box::new(expr2),
+        ));
     }
     println_with_indent(&format!("here4: {:?}", expressions));
 
     Ok((input, expressions.pop_front().unwrap()))
 }
 
-fn parse_lambda_application_simple(expr: &str) -> IResult<&str, LambdaExpression> {
-    inc_indent();
-    defer!(dec_indent());
-    println_with_indent("parsing_application_simple");
-    println_with_indent(&format!("here0: {:?}", expr));
-    let (input, variable) = take_while_m_n(1, 1, |c: char| c.is_alphabetic())(expr)?;
-    println_with_indent(&format!("here1: {:?}", input));
-
-    let (input, expr) = alt((
-        preceded(
-            opt(tag(" ")),
-            delimited(tag("("), parse_lambda_expression, tag(")")),
-        ),
-        preceded(tag(" "), parse_lambda_expression),
-    ))
-    .parse(input)?;
-    println_with_indent(&format!("here3: {:?}", input));
-
-    Ok((
-        input,
-        LambdaExpression::Application(
-            Box::new(LambdaExpression::Variable(variable.to_string())),
-            Box::new(expr),
-        ),
-    ))
-}
-
-fn parse_lambda_application_complex(expr: &str) -> IResult<&str, LambdaExpression> {
-    inc_indent();
-    defer!(dec_indent());
-    println_with_indent("parsing_application_complex");
-    println_with_indent(&format!("here0: {:?}", expr));
-    let (input, expr1) = delimited(tag("("), parse_lambda_expression, tag(")")).parse(expr)?;
-    println_with_indent(&format!("here1: {:?}", input));
-
-    let (input, expr2) = alt((
-        delimited(tag("("), parse_lambda_expression, tag(")")),
-        parse_lambda_expression,
-    ))
-    .parse(input)?;
-    println_with_indent(&format!("here2: {:?}", input));
-
-    Ok((
-        input,
-        LambdaExpression::Application(Box::new(expr1), Box::new(expr2)),
-    ))
-}
-
 fn parse_lambda_expression(expr: &str) -> IResult<&str, LambdaExpression> {
     inc_indent();
     defer!(dec_indent());
     println_with_indent("parsing_expression");
+    println_with_indent(&format!("here0: {:?}", expr));
     alt((
         parse_lambda_abstraction,
         parse_lambda_application,
@@ -252,7 +266,7 @@ mod tests {
     fn test_parse_lambda_application_simple() {
         indent_reset();
         let expr = String::from("x y");
-        let parsed_expr = parse_lambda_application_simple(&expr);
+        let parsed_expr = parse_lambda_application(&expr);
         assert!(parsed_expr.is_ok());
         let (input, expr) = parsed_expr.unwrap();
         assert_eq!(input, "");
@@ -297,7 +311,7 @@ mod tests {
     fn test_parse_lambda_application_complex_1() {
         indent_reset();
         let expr = String::from("(x y)z");
-        let parsed_expr = parse_lambda_application_complex(&expr);
+        let parsed_expr = parse_lambda_application(&expr);
         assert_eq!(
             parsed_expr,
             Ok((
@@ -317,7 +331,7 @@ mod tests {
     fn test_parse_lambda_application_complex_2() {
         indent_reset();
         let expr = String::from("(x y)(a b)");
-        let parsed_expr = parse_lambda_application_complex(&expr);
+        let parsed_expr = parse_lambda_application(&expr);
         assert_eq!(
             parsed_expr,
             Ok((
@@ -408,6 +422,48 @@ mod tests {
         )
     );
 
+    #[test]
+    fn test_parse_balanced_expression() {
+        indent_reset();
+        let expr = String::from("λx.f (x x))");
+        let (rest, parsed) = parse_string_balanced_expression(&expr).unwrap();
+        assert_eq!(rest, ")");
+        assert_eq!(parsed, "λx.f (x x)");
+    }
+
+    #[test]
+    fn test_parse_balanced_expression_2() {
+        indent_reset();
+        let expr = String::from("x x)");
+        let (rest, parsed) = parse_string_balanced_expression(&expr).unwrap();
+        assert_eq!(rest, ")");
+        assert_eq!(parsed, "x x");
+    }
+
+    test_parse_lambda_expression!(
+        y_part,
+        "(λx.x x)(λx.f (x x))",
+        LambdaExpression::Application(
+            Box::new(LambdaExpression::Abstraction(
+                "x".to_string(),
+                Box::new(LambdaExpression::Application(
+                    Box::new(LambdaExpression::Variable("x".to_string())),
+                    Box::new(LambdaExpression::Variable("x".to_string())),
+                ))
+            )),
+            Box::new(LambdaExpression::Abstraction(
+                "x".to_string(),
+                Box::new(LambdaExpression::Application(
+                    Box::new(LambdaExpression::Variable("f".to_string())),
+                    Box::new(LambdaExpression::Application(
+                        Box::new(LambdaExpression::Variable("x".to_string())),
+                        Box::new(LambdaExpression::Variable("x".to_string())),
+                    )),
+                ))
+            ))
+        )
+    );
+
     test_parse_lambda_expression!(
         y,
         "λf.(λx.x x)(λx.f (x x))",
@@ -459,13 +515,17 @@ mod tests {
         )
     );
 
-    test_parse_lambda_expression!(left_associative, "a b c", LambdaExpression::Application(
-        Box::new(LambdaExpression::Application(
-            Box::new(LambdaExpression::Variable("a".to_string())),
-            Box::new(LambdaExpression::Variable("b".to_string())),
-        )),
-        Box::new(LambdaExpression::Variable("c".to_string())),
-    ));
+    test_parse_lambda_expression!(
+        left_associative,
+        "a b c",
+        LambdaExpression::Application(
+            Box::new(LambdaExpression::Application(
+                Box::new(LambdaExpression::Variable("a".to_string())),
+                Box::new(LambdaExpression::Variable("b".to_string())),
+            )),
+            Box::new(LambdaExpression::Variable("c".to_string())),
+        )
+    );
 
     //test_parse_lambda_expression!(
     //    right_associative_2,
@@ -488,10 +548,10 @@ mod tests {
     //    )
     //);
 
-     //λn.λf.λx.
-     // n(λg.λh.h(g f))(λu.x)(λu.u)
+    //λn.λf.λx.
+    // n(λg.λh.h(g f))(λu.x)(λu.u)
     test_parse_lambda_expression!(
-        pred, 
+        pred,
         "λn.λf.λx.n(λg.λh.h(g f))(λu.x)(λu.u)",
         LambdaExpression::Abstraction(
             "n".to_string(),
@@ -531,4 +591,48 @@ mod tests {
             )),
         )
     );
+
+    test_parse_lambda_expression!(
+        function_application,
+        "f(f x)",
+        LambdaExpression::Application(
+            Box::new(LambdaExpression::Variable("f".to_string())),
+            Box::new(LambdaExpression::Application(
+                Box::new(LambdaExpression::Variable("f".to_string())),
+                Box::new(LambdaExpression::Variable("x".to_string())),
+            )),
+        )
+    );
+
+
+    #[test]
+    fn test_balanced_expression() {
+        indent_reset();
+        let expr = String::from("(x y)");
+        let parsed_expr = parse_string_balanced_expression(&expr);
+        assert!(parsed_expr.is_ok());
+        let (input, expr) = parsed_expr.unwrap();
+        assert_eq!(input, "");
+        assert_eq!(expr, "(x y)");
+    }
+
+    #[test]
+    fn test_application() {
+        indent_reset();
+        let expr = String::from("f(x y)");
+        let parsed_expr = parse_lambda_application(&expr);
+        assert_eq!(
+            parsed_expr,
+            Ok((
+                "",
+                LambdaExpression::Application(
+                    Box::new(LambdaExpression::Variable("f".to_string())),
+                    Box::new(LambdaExpression::Application(
+                        Box::new(LambdaExpression::Variable("x".to_string())),
+                        Box::new(LambdaExpression::Variable("y".to_string())),
+                    )),
+                )
+            ))
+        );
+    }
 }
